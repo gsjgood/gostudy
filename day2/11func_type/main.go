dfe4888c42e60d1bd68caf913c2ff8b67f4721f7
geshuaijun <geshuaijun@baichebao.com>
2021-11-25 21:13:25 +0800

    更新代码

--- day2/11func_type/main.go
+++ day2/11func_type/main.go
@@ -1,1 +1,21 @@
-package _1func_type
+package main
+
+import "fmt"
+
+func f1() int {
+	return 10
+}
+//函数可以作为参数类型
+func f2(x func()int)  {
+	ret := x()
+
+	fmt.Println(ret)
+}
+func main() {
+	a := f1
+	fmt.Printf("type:%T\n", a)
+
+	f2(a)
+	f2(f1)
+
+}
